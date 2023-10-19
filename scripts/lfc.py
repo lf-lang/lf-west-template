f9540a8489d5edcca5ec7f7310a2a9c6c85ed547
erlingrj <erling.jellum@gmail.com>
2023-10-19 23:20:56 +0200

    Reorganize template

--- scripts/lfc.py
+++ scripts/lfc.py
@@ -3,6 +3,7 @@ from west.commands import WestCommand  # your extension must subclass this
 from west import log                   # use this for user output
 
 import os
+import shutil
 import subprocess
 
 class Lfc(WestCommand):
@@ -29,9 +30,27 @@ class Lfc(WestCommand):
 
     def do_run(self, args, unknown_args):
         
+        # Verify that the project is laid out correctly
         if "src" not in args.app.split("/"):
-            print("ERROR: West lf-build must be invoked outside `src` folder")
+            print("ERROR: West lfc must be invoked outside `src` folder")
 
+        # Your input file path
+        # file_path = "/path/to/old_directory/filename.txt"
+
+        # Remove the file extension
+        # file_name, file_extension = os.path.splitext(file_path)
+        # file_name = os.path.basename(file_name)
+
+        # Replace the directory name
+        # new_directory_name = "new_directory"
+        # file_path = file_path.replace("old_directory", new_directory_name)
+
+        # Combine the modified path with the new directory name
+        # new_path = os.path.join(os.path.dirname(file_path), file_name + file_extension)
+
+        # print(new_path)
+
+        # Find the path where lfc will put the generated sources
         srcGenPath = args.app.split(".")[0].replace("src", "src-gen")
         appPath = args.app.split("src")[0]
         if appPath == "":
@@ -49,20 +68,11 @@ class Lfc(WestCommand):
             exit(1)
         
         # 2. Copy prj.conf and Kconfig into the src-gen folder
-        res = subprocess.Popen(f"cp {appPath}/Kconfig {srcGenPath}/", shell=True)
-        ret = res.wait()
-        if ret != 0:
-            exit(1)
-
-        res = subprocess.Popen(f"cp {appPath}/prj.conf {srcGenPath}/", shell=True)
-        ret = res.wait()
-        if ret != 0:
-            exit(1)
+        shutil.copyfile(f"{appPath}/Kconfig", f"{srcGenPath}/Kconfig")
+        shutil.copyfile(f"{appPath}/prj.conf", f"{srcGenPath}/prj.conf")
         
         # Invoke west in the `src-gen` directory. Pass in 
-
         if args.build:
-          print(args.build)
           westCmd = f"west build {srcGenPath} {args.build}"
           print(f"Executing west command: `{westCmd}`")
           res = subprocess.Popen(westCmd, shell=True)
